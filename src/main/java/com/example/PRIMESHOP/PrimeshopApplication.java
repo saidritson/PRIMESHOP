5bb7aa25ddf8d9a380f472a558b6b652654bfa12
DarioCortezITSON252267 <dario.cortez252267@potros.itson.edu.mx>
2026-06-22 16:33:02 -0700

    Dependencias

---
+++ src/main/java/com/example/PRIMESHOP/PrimeshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.PRIMESHOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PRIMESHOP/PrimeshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PRIMESHOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
